e792be382e531207c8f3043915112174eb25640a
Lubosz Sarnecki <lubosz@gmail.com>
2021-11-30 10:43:51 +0100

    figure out julian days from dt in tests.

--- test/test_mx_time.py
+++ test/test_mx_time.py
@@ -19,6 +19,15 @@ class UTC(tzinfo):
         return timedelta(0)
 
 
+def datetime_to_days(dt):
+    delta = dt - datetime(1, 1, 1)
+    return delta.total_seconds() / (60 * 60 * 24)
+
+
+def datetime_to_julian_days(dt):
+    return datetime_to_days(dt) + 1721424.5 + 1
+
+
 class MxTimeTest(unittest.TestCase):
     def test_construction(self):
         dt = DateTime.DateTime(1900, 1, 1, 12)
@@ -78,6 +87,53 @@ class MxTimeTest(unittest.TestCase):
 
         d = DateTime.DateTimeFrom(year=2039, month=7, day=29)
         print("dt_mx unix2", d.ticks())
+
+        datetime.utcfromtimestamp(2195510400)
+
+    def test_days(self):
+        dt_mx = DateTime.DateTimeFrom(year=2039, month=7, day=29)
+        print("dt_mx days", dt_mx.absdays)
+        print("dt_mx julian days", dt_mx.jdn)
+
+        dt_local = datetime(2039, 7, 29)
+
+        print("dt_local days", datetime_to_days(dt_local))
+        print("dt_local julian days", datetime_to_julian_days(dt_local))
+
+        utc_unix = calendar.timegm((2039, 7, 29, 0, 0, 0))
+        dt_utc = datetime.utcfromtimestamp(utc_unix)
+
+        print("dt_utc days", datetime_to_days(dt_utc))
+        print("dt_utc julian days", datetime_to_julian_days(dt_utc))
+
+    def test_jd_calc(self):
+        year = 1900
+        month = 7
+        day = 29
+        hour = 11
+        minute = 12
+        second = 23
+
+        for i in range(1000):
+            dt_mx = DateTime.DateTimeFrom(year=year, month=month, day=day,
+                                          hour=hour, minute=minute, second=second)
+
+            dt = datetime(year, month, day, hour, minute, second)
+
+            assert round(dt_mx.jdn, 8) == round(datetime_to_julian_days(dt), 8)
+
+            year += 3
+            month += 3
+            day += 3
+            hour += 3
+            minute += 3
+            second += 3
+
+            month = month % 12 + 1
+            day = day % 28 + 1
+            hour = hour % 24
+            minute = minute % 60
+            second = second % 60
 
 
 if __name__ == "__main__":
